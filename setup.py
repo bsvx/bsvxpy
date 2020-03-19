8aef5d3c32efb4e5d4ddbde76e4b4b0708c5f6d7
NoahMezher <noahmezher@gmail.com>
2020-03-19 15:58:25 -0400

    Update setup.py and tests folder for pip module implementation

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name = 'bsvxpy',
     version = '0.1',
-    packages = find_packages(),
+    packages = find_packages(exclude='tests'),
     long_description = open('README.md').read()
 )
---
+++ tests/test.py
@@ -0,0 +1,6 @@
+import bsvxpy
+
+# here's the syntax on how to call the objects
+
+a = bsvxpy.bsvxBlank.Blank()
+print (a)
