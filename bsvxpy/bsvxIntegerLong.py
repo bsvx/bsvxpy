59516a0b0b670eb8d756f0fa349b92253d1c3c6f
Jack Summers <jackrsummers@gmail.com>
2020-03-23 15:38:06 -0400

    Update bsvxIntegerLong.py

--- bsvxpy/bsvxIntegerLong.py
+++ bsvxpy/bsvxIntegerLong.py
@@ -3,12 +3,14 @@
 from .bsvxIntegerShort import IntegerShort
 
 class IntegerLong(IntegerShort):
-    _long_length = None
 
     def __init__(self, input):
+        # Long int = 144 + [0,7]
         mask = int('111', 2)
-        self._long_length = (input & mask) + 1
 
-        self._length = int(self.read(None, self._long_length), 16)
+        # long integers will use 1-8 bytes of data to represent its value
+        # (input & mask) will map to integer values [0 - 7], + 1 to shift the range
+        self._length = (input & mask) + 1
+
         #todo: zig-zag decoding
         self._data = int(self.read(None, self._length), 16)
