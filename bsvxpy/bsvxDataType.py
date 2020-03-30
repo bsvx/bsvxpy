12ee8de7471763dc395d4a8f587915036e79b365
Jack Summers <jackrsummers@gmail.com>
2020-03-30 15:15:49 -0400

    Added _hex_data private variable
    _hex_data stores the raw hex to/from BSVX files. Changed _data private variable to store non-hex representation of data, actual type varies on subclass.

--- bsvxpy/bsvxDataType.py
+++ bsvxpy/bsvxDataType.py
@@ -3,8 +3,9 @@
 class bsvxDataType:
     # Private Member variables
     # ------------------------
-    _length = None  # (int, base 10), the number of bytes we need to read after reading the size bit 
-    _data = None    # (int, base 16) the hex representation of the data
+    _length = None  # (int, base 10) the number of bytes we need to read after reading the size bit 
+    _hex_data = None # (int, base 16) the hex representation of the data
+    _data = None    # (Subclass dependent) the data representation in the "correct" type, i.e. int for ShortInt, string for ShortString, etc.
     _mask = None    # (int, base 2) corresponding bits in the input byte used to encode length or data
     
     # Constructor
