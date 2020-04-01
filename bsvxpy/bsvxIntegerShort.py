f7e0195eeacb951eb3cdc04b8ade1a4ee0996326
Jack Summers <jackrsummers@gmail.com>
2020-04-01 11:09:00 -0400

    Fixed IntegerShort to correctly assign value to _data variable

--- bsvxpy/bsvxIntegerShort.py
+++ bsvxpy/bsvxIntegerShort.py
@@ -6,8 +6,11 @@ class IntegerShort(bsvxDataType):
     # Small Int = 136 + [0, 7] : An integer in the range 0-7
     _mask = int('111', 2)
 
+    # IntegerShort's _data and _hex_data values will be identical due to binary encoding of decimal integers
+    # This results in no decoding b/w _hex_data and _data assignments.
+    # __init__ structure preserved for congruency throughout library
     def __init__(self, input):
-        self._length = 0                # Small Int does not read data from file
-        self._hex_data = input & self._mask # data is the 0 - 7 value encoded in the input byte
-        self._data = int(self._hex_data, 16)
+        self._length = 0                    # Short Int does not read data from file
+        self._hex_data = input & self._mask # store the raw hexadecimal data in _hex_data
+        self._data = self._hex_data         # data is the 0 - 7 value encoded in the input byte
         return
